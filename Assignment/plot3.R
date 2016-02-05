92530ca5cb89e6345813666598a831e8d0a9c0da
Nawal Bihari Sarda <nawal.bihari.sarda@ericsson.com>
2016-02-05 11:26:17 +0530

    Assignment1

---
+++ Assignment/plot3.R
@@ -0,0 +1,11 @@
+png("plot3.png", width = 480, height = 480, units = "px", bg = "white")
+par(mar= c(4, 4, 2, 1))
+with(data, {
+    plot(Sub_metering_1 ~ Datetime, type = "l", 
+    ylab = "Global Active Power (kilowatts)", xlab = "")
+    lines(Sub_metering_2 ~ Datetime, col = 'Red')
+    lines(Sub_metering_3 ~ Datetime, col = 'Blue')
+})
+legend("topright", col = c("black", "red", "blue"), lty = 1, lwd = 2, 
+       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+dev.off()
